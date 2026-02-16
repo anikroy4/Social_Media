fc9e307ccc2cafc67f723af294d53cf072a8af7b
Anik Roy <anikroy.uiu@gmail.com>
2026-02-16 18:48:39 +0600

    changes of login and creating login
    changes of login and creating login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import './App.css'
 import Registration from '../src/pages/registration/Registration'
+import Login from './pages/login/Login'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route >
+    <Route  path='/' element={<Login />}>
       <Route path='/registration' element={<Registration />} />
     </Route>
   )
@@ -16,7 +17,6 @@ const router = createBrowserRouter(
 
 function App() {
   
-
   return (
     <>
       <RouterProvider router={router} />
